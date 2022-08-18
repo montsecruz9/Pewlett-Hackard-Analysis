2a435eac423392c29ad08b5c92822df68b574d13
montsecruz9 <montsecruzg9@gmail.com>
2022-08-18 17:55:33 -0500

    Updating deliverable 1 queries

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -6,9 +6,32 @@ SELECT e.emp_no,
 	titles.title,
 	titles.from_date,
 	titles.to_date
---INTO retiring_employees
+INTO retiring_employees
 FROM employees as e
 LEFT JOIN titles
 ON (e.emp_no = titles.emp_no)
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	titles.title,
+	titles.from_date,
+	titles.to_date
+INTO unique_titles
+FROM employees as e
+INNER JOIN titles
+ON (e.emp_no = titles.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (titles.to_date = '9999-01-01')
+ORDER BY e.emp_no, titles.from_date DESC;
+
+-- Number of employees retiring by title
+SELECT COUNT(title),
+	title
+INTO retiring_titles_count
+FROM unique_titles 
+GROUP BY title
+ORDER BY COUNT(title) DESC;
