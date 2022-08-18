8c2697e8c01f08a57e1a654da34edb245373f8ab
montsecruz9 <montsecruzg9@gmail.com>
2022-08-18 18:17:26 -0500

    Updating challenge queries

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -35,3 +35,21 @@ INTO retiring_titles_count
 FROM unique_titles 
 GROUP BY title
 ORDER BY COUNT(title) DESC;
+
+-- DELIVERABLE 2
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	titles.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles
+ON (e.emp_no = titles.emp_no)
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (titles.to_date = '9999-01-01')
+ORDER BY e.emp_no;
